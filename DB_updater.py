43a922d396dd94a6297a8b8f884df07be90b726d
SakuraSa <sakura_kyon@hotmail.com>
2014-05-19 11:41:48 +0800

    fix bugs 不再在开始运行时清除API cache

--- DB_updater.py
+++ DB_updater.py
@@ -17,10 +17,6 @@ version   = "ver0.001"
 database  = sqlite3.connect(dbname)
 cursor    = database.cursor()
 
-#clear the API chache
-cursor.execute(r'delete from statistics_cache')
-database.commit()
-
 #get database version and do backup
 if cursor.execute(r"SELECT COUNT(*) as CNT FROM sqlite_master where type='table' and name='dbupdate';").fetchall()[0][0]:
     version = cursor.execute(r"SELECT value FROM dbupdate where key='version' limit 1").fetchall()[0][0]
